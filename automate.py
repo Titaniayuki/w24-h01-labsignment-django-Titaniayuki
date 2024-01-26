c0f8b1860bd1ee20c1bebb34201379cb93887bdf
titaniayuki <hettiara@ualberta.ca>
2024-01-25 22:09:28 -0700

    automate changes

--- automate.py
+++ automate.py
@@ -1,9 +1,20 @@
 import venv
 import os
+import subprocess
 
-venv_dir = os.path.join(os.getcwd(), "venv2")
-print(venv_dir)
-venv.create(venv_dir)
-full_path = os.path.join("/home", "Scripts/activate")
-print("this: " + full_path)
-#exec(open(os.path.join(venv_dir, "/Scripts/activate")).read())
+venv_dir = os.path.join(os.getcwd(), "venv")
+full_path = os.path.join("venv", "bin", "activate")
+print("first: "+ full_path)
+#venv.create(venv_dir)
+
+subprocess.Popen("virtualenv venv --python=python3")
+
+#subprocess.run("source venv/bin/activate")
+
+subprocess.Popen(full_path, shell=True)
+subprocess.Popen("pip install -r requirements.txt", shell=True);
+lab2_path = os.path.join(os.getcwd(), "lab2")
+subprocess.Popen("npm install", shell=True, cwd=lab2_path);
+subprocess.Popen("npx esbuild main.js --bundle --minify --sourcemap --outfile=./emojis/static/main.min.js", shell=True, cwd=lab2_path);
+
+
